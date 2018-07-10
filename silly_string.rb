434af6a3898bb4803598e0b506026d32cf54e1b4
Kelly Cotter <kjc853@gmail.com>
2018-07-10 12:44:43 +0000

    Done.

--- silly_string.rb
+++ silly_string.rb
@@ -1,19 +1,22 @@
 
 # Challenge 1: Fill this string in with your favorite word
 
-your_favorite_word = ""
+your_favorite_word = "kelly"
 
 puts "My favorite word is #{your_favorite_word}"
 
 # Challenge 2: Capitalize every letter in your_favorite_word using a built in Ruby method
 
 favorite_word_in_all_caps = your_favorite_word # Put a . after your_favorite word and use a String method.
+puts "kelly".upcase
 
-puts "MY FAVORITE WORD IS #{favorite_word_in_all_caps}"
+puts "MY FAVORITE WORD IS #{KELLY}"
 
 # Challenge 3: Make all the letters of phrase lowercase using a built in Ruby method
 
 phrase = "IM NOT SHOUTING"
+
+puts "#{phrase}".downcase
 
 lowercased_phrase = phrase # Put a . after phrase and use a String method.
 
@@ -24,12 +27,13 @@ puts lowercased_phrase
 big_word = "supercalifragilisticexpialidocious"
 
 letter_count = big_word # Put a . after big_word and use a String method.
-
+big_word="supercalifragilisticexpialidocious"
+letter_count= puts "#{big_word}".length
 puts "There are #{letter_count} in #{big_word}"
-
+puts "supercalifragilisticexpialidocious".length
 # Challenge 5: Add "wow" and "mom" to big_word
 
-bigger_word = big_word # Use String concatenation to add "wow" and "mom" to big_word
+bigger_word = big_word+ #{wow+mom}
 
 new_letter_count = bigger_word.size
 
@@ -39,13 +43,13 @@ puts "There are now #{new_letter_count} in #{bigger_word}"
 
 proper_sentence = "i really like programming." # Put a . after the string to capitalize it properly.
 
-puts proper_sentence
+puts "proper_sentence".capitalize
 
 # Challenge 7: String Switcheroo
 sentence = "Astounding aardvarks, arguably an ancient animal, always ate apples and acorns and artichokes all around Athens, amazing!"
 
 # Swap all `A` for `O`, as well as all `a` for `o`. You need to chain `gsub`s.
 
-sentence = sentence # Add your gsub chain here
+sentence = "sentence"
 
-puts sentence
+puts "sentence".gsub("a","o"),("o","a")
