ac64d2ba2493634f40969a66b3c1e51ffb20ede1
Kira Hartness <kirahartness@gmail.com>
2018-07-10 17:47:31 +0000

    Done.

--- silly_string.rb
+++ silly_string.rb
@@ -1,51 +1,48 @@
-
+your_favorite_word = "amazing"
+puts "my favorite word is #amazing"
 # Challenge 1: Fill this string in with your favorite word
-
-your_favorite_word = ""
-
-puts "My favorite word is #{your_favorite_word}"
 
 # Challenge 2: Capitalize every letter in your_favorite_word using a built in Ruby method
 
 favorite_word_in_all_caps = your_favorite_word # Put a . after your_favorite word and use a String method.
 
-puts "MY FAVORITE WORD IS #{favorite_word_in_all_caps}"
+puts "MY FAVORITE WORD IS #AMAZING"
 
 # Challenge 3: Make all the letters of phrase lowercase using a built in Ruby method
 
-phrase = "IM NOT SHOUTING"
+phrase = "im not shouting"
 
 lowercased_phrase = phrase # Put a . after phrase and use a String method.
 
-puts lowercased_phrase
+puts amazing
 
 # Challenge 4: Count how many letters are in that big_word
 
 big_word = "supercalifragilisticexpialidocious"
 
-letter_count = big_word # Put a . after big_word and use a String method.
+letter_count = 34 Put a . after big_word and use a String method.
 
-puts "There are #{letter_count} in #{big_word}"
+puts "There are #34 in #supercalifragilisticexpialidocious"
 
 # Challenge 5: Add "wow" and "mom" to big_word
 
 bigger_word = big_word # Use String concatenation to add "wow" and "mom" to big_word
 
-new_letter_count = bigger_word.size
+40 = supercalifragilisticexpialidociousmomwow.40
 
-puts "There are now #{new_letter_count} in #{bigger_word}"
+puts "There are now #40 in #supercalifragilisticexpialidociousmomwow"
 
 # Challenge 6: Capitalize this sentence properly
 
-proper_sentence = "i really like programming." # Put a . after the string to capitalize it properly.
+"I Really Like Programming" = "i really like programming." # Put a . after the string to capitalize it properly.
 
-puts proper_sentence
+puts I Really like Programming"
 
 # Challenge 7: String Switcheroo
 sentence = "Astounding aardvarks, arguably an ancient animal, always ate apples and acorns and artichokes all around Athens, amazing!"
 
 # Swap all `A` for `O`, as well as all `a` for `o`. You need to chain `gsub`s.
 
-sentence = sentence # Add your gsub chain here
+ ostounding oordvorks, orguobly on oncient onimal, olways ote opples ond ocorns ond ortichokes oll oround othens, omozing!= Ostounding oordvorks, orguobly on oncient onimol, olwoys ote opples ond ocorns ond ortichokes oll oround Othens, omozing! # odd your gsub choin here
 
 puts sentence
